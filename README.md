780d6d96a4746e5c89609d70ac8f9e953014e6f6
fczarnos <ffczarnos@gmail.com>
2026-05-25 22:09:30 -0300

    Criadas classes Dinheiro, Moeda, interrupções e testes

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
-# Comum.Financeiro
+# Comum.Financeiro2
 Este tutorial que apresenta o passo a passo para iniciar o projeto Comum.Financeiro a partir de um repositório criado no GitHub e clonado para o computador local.
-
-teste3
---
+++ tests/Comum.Financeiro.Tests/DineheiroTests.cs
@@ -0,0 +1,98 @@
+using Comum.Financeiro;
+
+public class DinheiroTests
+{
+    [Fact]
+    public void DeveCriarDinheiroEmReais()
+    {
+        var dinheiro = Dinheiro.EmReais(10.50m);
+
+        // Teste de integridade
+        /*Assert.Equal(11.50m, dinheiro.Valor);
+        Assert.Equal(Moeda.RealBrasileiro, dinheiro.Moeda);*/
+    }
+    
+    [Fact]
+    public void DeveSomarDoisValoresemBrl()
+    {
+        var primeiroValor = Dinheiro.EmReais(10m);
+        var segundoValor = Dinheiro.EmReais(5.50m);
+
+        var resultado = primeiroValor.Somar(segundoValor);
+
+        Assert.Equal(15.50m, resultado.Valor);
+        Assert.Equal(Moeda.RealBrasileiro, resultado.Moeda);
+    }
+
+    [Fact]
+    public void DeveSubtrairDoisValoresEmBrl()
+    {
+        var primeiroValor = Dinheiro.EmReais(12m);
+        var segundoValor = Dinheiro.EmReais(7.50m);
+
+        var resultado = primeiroValor.Subtrair(segundoValor);
+
+        Assert.Equal(4.50m, resultado.Valor);
+        Assert.Equal(Moeda.RealBrasileiro, resultado.Moeda);
+    }
+
+    [Fact]
+    public void DeveImpedirSomaEntreBrlEUsd()
+    {
+        var valorReal = Dinheiro.EmReais(5m);
+        var valorDolar = Dinheiro.EmDolar(5m);
+
+        Assert.Throws<MoedasDiferentesException>(()=>valorReal.Somar(valorDolar));
+    }
+
+    [Fact]
+
+    public void DeveMultiplicarDinheiroPorQuantidade()
+    {
+        var deixReaix = Dinheiro.EmReais(10m);
+
+        var resultado = deixReaix.Multiplicar(5);
+
+        Assert.Equal(50m, resultado.Valor);
+        Assert.Equal(Moeda.RealBrasileiro, resultado.Moeda);
+    }
+
+    [Fact]
+    public void DeveVerificarValorEhZero()
+    {
+        var dinheiro = Dinheiro.EmReais(0m);
+        //var valor2 = Dinheiro.EmReais(10m);
+
+        //var resultado = valor1.Subtrair(valor2);
+
+        //Assert.Equal(0m, resultado.Valor);
+        Assert.True(dinheiro.EhZero());
+        Assert.False(dinheiro.EhPositivo());
+        Assert.False(dinheiro.EhNegativo());
+    }
+
+    [Fact]
+    public void DeveVerificarValorEhPositivo()
+    {
+        //var valNegativo = Dinheiro.EmReais(-5m);
+        var valPositivo = Dinheiro.EmReais(10m);
+
+        //var resultado = valNegativo.Somar(valPositivo);
+
+        //Assert.Equal(5m, resultado.Valor);
+
+        Assert.False(valPositivo.EhZero());
+        Assert.True(valPositivo.EhPositivo());
+        Assert.False(valPositivo.EhNegativo());
+    }
+
+    [Fact]
+    public void DeveFormatarValorEmReais()
+    {
+        var dinheiro = Dinheiro.EmReais(10.5m);
+        var valorFormatado = dinheiro.Formatar();
+
+        Assert.Contains("R$", valorFormatado);
+        Assert.Contains("10,50", valorFormatado);
+    }
+}
